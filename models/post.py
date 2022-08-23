ec18edd3d3cfdae3edc7db045780b526505baab3
Alexandra Ferrari <alexandra.ferrari@student.ipso.ch>
2022-08-23 22:00:59 +0200

    Added basic registration setup

--- models/post.py
+++ models/post.py
@@ -1,4 +1,5 @@
-from sqlalchemy import func
+from datetime import datetime
+import pytz
 
 from app import db
 
@@ -7,9 +8,9 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String, unique=True, nullable=False)
     body = db.Column(db.Text, nullable=False)
-    posted_at = db.Column(db.DateTime(timezone=True), nullable=False, default=func.now())
+    posted_at = db.Column(db.DateTime(), nullable=False, default=lambda: datetime.now(pytz.timezone("Europe/Zurich")))
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    user = db.relationship("User", back_populates="posts", )
+    user = db.relationship("User", back_populates="posts")
 
     def __repr__(self):
         return '<Post %r>' % self.title
--- routes/auth.py
+++ routes/auth.py
@@ -1,12 +1,13 @@
 from flask import render_template, redirect, flash, request
 from flask_login import login_required, logout_user, login_user
 from flask_wtf import FlaskForm
+from sqlalchemy import or_
 from sqlalchemy.orm import joinedload
 from wtforms import PasswordField, BooleanField, StringField, SubmitField
-from wtforms.validators import Email, Length, InputRequired
+from wtforms.validators import Email, Length, InputRequired, EqualTo
 
-from app import app, login_manager
-from models import User
+from app import app, login_manager, db
+from models import User, Role
 
 
 class LoginForm(FlaskForm):
@@ -18,6 +19,20 @@ class LoginForm(FlaskForm):
 
     def __init__(self, *args, **kwargs):
         super(LoginForm, self).__init__(*args, **kwargs)
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Benutzername',
+                           validators=[InputRequired("Bitte geben Sie einen Benutzername an"), Length(1, 64)])
+    email = StringField('Email',
+                        validators=[InputRequired("Bitte geben Sie eine Mailadresse an"), Length(1, 64), Email()])
+    password = PasswordField("Passwort",
+                             validators=[InputRequired(), EqualTo('confirm_password', message='Passwords must match')])
+    confirm_password = PasswordField("Passwort bestätigen", validators=[InputRequired()])
+    submit = SubmitField("Registrieren")
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
 
 
 @login_manager.user_loader
@@ -43,3 +58,22 @@ def login():
         else:
             flash("Ungültige Logindaten")
     return render_template('login.html', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User.query.filter(or_(User.email == form.email.data, User.username == form.username.data)).first()
+        if user:
+            flash("Ungültige Daten")
+        else:
+            email = form.email.data
+            username = form.username.data
+            password = form.password.data
+            role = Role.query.filter_by(name="user").first()
+            new_user = User(email=email, username=username, password=password, role=role)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect("/login")
+    return render_template('register.html', form=form)
