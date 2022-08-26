3d49324c539c85788c17b9ae036a511c8d738fef
Alexandra Ferrari <alexandra.ferrari@student.ipso.ch>
2022-08-26 21:39:07 +0200

    Added seed_db cli command

--- app.py
+++ app.py
@@ -13,28 +13,4 @@ login_manager.login_view = "login"
 
 from routes import *
 from models import *
-
-db.drop_all()
-db.create_all()
-
-db.session.bulk_save_objects([
-    Role(name="admin"),
-    Role(name="user")
-])
-db.session.commit()
-
-# bulk_save_objects won't trigger events (user => before_insert)
-db.session.add(
-    User(email="admin@admin.ch", password="password", username="admin", role_id=1)
-)
-db.session.add(
-    User(email="user@user.ch", password="password", username="user", role_id=2)
-)
-db.session.commit()
-
-db.session.bulk_save_objects([
-    Post(title="test post", body="post bodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybody", user_id=1),
-    Post(title="test post 2", body="post bodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybody", user_id=1),
-    Post(title="test post 3", body="post bodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybodybody", user_id=1)
-])
-db.session.commit()
+from commands import *
---
+++ commands.py
@@ -0,0 +1,33 @@
+from app import app, db
+from models import Role, User, Post
+
+
+@app.cli.command("seed-db")
+def seed_db():
+    db.drop_all()
+    db.create_all()
+
+    db.session.bulk_save_objects([
+        Role(name="admin"),
+        Role(name="user")
+    ])
+    db.session.commit()
+
+    # bulk_save_objects won't trigger events (user => before_insert)
+    db.session.add(
+        User(email="admin@admin.ch", password="password", username="admin", role_id=1)
+    )
+    db.session.add(
+        User(email="user@user.ch", password="password", username="user", role_id=2)
+    )
+    db.session.commit()
+
+    db.session.bulk_save_objects([
+        Post(title="First post of the page",
+             body="orem ipsum dolor sit amet, consectetur adipiscing elit. Aenean diam justo, accumsan vel pretium ac, cursus quis arcu. Nulla cursus quam eu enim pharetra convallis. Nunc elementum ipsum ac scelerisque molestie. Ut at augue ultrices, aliquet tellus quis, fermentum nunc. Maecenas sed nulla non erat sodales vulputate. Etiam et neque tortor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque nec placerat purus, eu efficitur elit. Curabitur vel faucibus lectus. Praesent venenatis nunc in ultrices mattis. Pellentesque dui nibh, dignissim id viverra non, rutrum nec orci. Vestibulum a ex dui.", user_id=2),
+        Post(title="Second one",
+             body="orem ipsum dolor sit amet, consectetur adipiscing elit. Aenean diam justo, accumsan vel pretium ac, cursus quis arcu. Nulla cursus quam eu enim pharetra convallis. Nunc elementum ipsum ac scelerisque molestie. Ut at augue ultrices, aliquet tellus quis, fermentum nunc. Maecenas sed nulla non erat sodales vulputate. Etiam et neque tortor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque nec placerat purus, eu efficitur elit. Curabitur vel faucibus lectus. Praesent venenatis nunc in ultrices mattis. Pellentesque dui nibh, dignissim id viverra non, rutrum nec orci. Vestibulum a ex dui.", user_id=1),
+        Post(title="Last one",
+             body="orem ipsum dolor sit amet, consectetur adipiscing elit. Aenean diam justo, accumsan vel pretium ac, cursus quis arcu. Nulla cursus quam eu enim pharetra convallis. Nunc elementum ipsum ac scelerisque molestie. Ut at augue ultrices, aliquet tellus quis, fermentum nunc. Maecenas sed nulla non erat sodales vulputate. Etiam et neque tortor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque nec placerat purus, eu efficitur elit. Curabitur vel faucibus lectus. Praesent venenatis nunc in ultrices mattis. Pellentesque dui nibh, dignissim id viverra non, rutrum nec orci. Vestibulum a ex dui.", user_id=2),
+    ])
+    db.session.commit()
