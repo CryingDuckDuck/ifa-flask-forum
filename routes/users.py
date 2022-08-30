031b1f27cba10e70fecc490bee1c01ade74920f3
Alexandra Ferrari <alexandra.ferrari@student.ipso.ch>
2022-08-30 21:33:04 +0200

    Finished user management and cleaned up error messages

--- routes/users.py
+++ routes/users.py
@@ -1,8 +1,21 @@
-from flask import render_template, abort
+from flask import render_template, abort, flash
 from flask_login import login_required, current_user
+from flask_wtf import FlaskForm
+from sqlalchemy.orm import joinedload
+from wtforms import SubmitField, IntegerField, SelectField
+from wtforms.widgets import HiddenInput
 
-from app import app
-from models import Post
+from app import app, db
+from models import Post, User, Role
+
+
+class UserRoleForm(FlaskForm):
+    user_id = IntegerField(widget=HiddenInput())
+    role = SelectField("Role", coerce=int)
+    submit = SubmitField("Role ändern")
+
+    def __init__(self, *args, **kwargs):
+        super(UserRoleForm, self).__init__(*args, **kwargs)
 
 
 @app.route("/profile")
@@ -18,4 +31,20 @@ def manage_users():
     if not current_user.is_admin:
         return abort(401)
 
-    return render_template("users/manage.html")
+    form = UserRoleForm()
+    roles = Role.query.all()
+    form.role.choices = [(r.id, r.name) for r in roles]
+
+    if form.validate_on_submit():
+        user = User.query.get(form.user_id.data)
+
+        if user is None:
+            return abort(404)
+
+        user.role_id = form.role.data
+        db.session.commit()
+        form.role.data = 0
+        flash(f"Role des Benutzers {user.username} wurde geändert!")
+
+    users = User.query.options(joinedload("role")).all()
+    return render_template("users/manage.html", users=users, form=form)
